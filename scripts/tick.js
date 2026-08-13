5b110b99778ab2c0fa6d697f2cef3a1778146b39
Shante Shannon <shantesheree90@icloud.com>
2026-08-12 21:52:49 -0400

    speed and stop experiments

--- scripts/tick.js
+++ scripts/tick.js
@@ -1,8 +1,10 @@
 let count = 0;
+let timer = setInterval(tick,1000);
 
 function tick() {
-  count = count + 1;
-  console.log("Tick " + count);
+	count = count +1;
+	console.log(count);
+	
+if (count=== 10){
+clearInterval(timer);}
 }
-
-setInterval(tick, 1000);
