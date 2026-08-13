2ada0e3376f5ad887bac10e81019ce1be9a0faa6
Shante Shannon <shantesheree90@icloud.com>
2026-08-12 22:13:21 -0400

    random closing

--- scripts/madlibs.js
+++ scripts/madlibs.js
@@ -2,16 +2,24 @@ let name = prompt("A name:");
 let adjective = prompt("An adjective:");
 let noun = prompt("A noun:");
 
+let closings = [
+  "You are a legend.",
+  "Stay golden.",
+  "Signs point to awesome.",
+  "You are kind."
+  "You are smart."
+  "You are important."
+];
+
+function randomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a + 1)) + a;
+}
+
 function makeCard() {
   console.log("Dear " + name + ",");
   console.log("You are as " + adjective + " as a " + noun + ".");
-  console.log("Happy birthday from the generator.");
+  let index = randomNumber(0, closings.length - 1);
+  console.log(closings[index]);
 }
 
-function makeThankYouCard() {
-  console.log("Dear " + name + ",");
-  console.log("Thanks a " + noun + " — that was " + adjective + " of you.");
-  console.log("Sincerely, the generator.");
-}
 makeCard();
-makeThankYouCard();
